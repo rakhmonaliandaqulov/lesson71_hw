6475b7306189b4e684e03fb7fca5b98d7d6416ae
RakhmonaliAnaqulov <rakhmonaliandaqulov@gmail.com>
2023-04-01 17:23:12 +0500

    Initial commit

---
+++ src/main/java/com/example/Lesson71HwApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson71HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson71HwApplication.class, args);
+	}
+
+}
